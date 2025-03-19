f1b041422308c5b15e3e5dca92f6f6d95630f670
gitgitgitgithub1 <162154543+gitgitgitgithub1@users.noreply.github.com>
2025-03-19 11:40:02 +0800

    修改eleventy文件

--- .eleventy.js
+++ .eleventy.js
@@ -5,12 +5,11 @@ module.exports = function (eleventyConfig) {
 
   return {
     // markdownTemplateEngine: "njk", // 如果需要使用 Nunjucks 作为 Markdown 模板引擎，可以取消注释
-    templateFormats: ["html", "njk", "md"], // 支持的模板格式
-    pathPrefix: "/QiyuanWarehouse/", // 设置路径前缀
+    // templateFormats: ["html", "njk", "md"], // 支持的模板格式
     dir: {
       input: "src",    // 输入文件夹
       output: "dist",  // 输出文件夹
-      includes: "includes", // 模板文件存放目录
+      // includes: "includes", // 模板文件存放目录
     },
   };
 };
