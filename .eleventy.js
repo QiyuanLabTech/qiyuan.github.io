dbd2758acb42e2d1c9d0cffa62d59342c93a6d87
gitgitgitgithub1 <162154543+gitgitgitgithub1@users.noreply.github.com>
2025-03-20 10:24:16 +0800

    更新网站

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,40 @@
+// module.exports = function (eleventyConfig) {
+//   // 复制静态资源（CSS、JS）到输出目录
+//   eleventyConfig.addPassthroughCopy("src/css");
+//   eleventyConfig.addPassthroughCopy("src/js");
+//   eleventyConfig.addPassthroughCopy("src/images");
+//   return {
+//     dir: {
+//       input: "src",    // 输入文件夹
+//       output: "dist",  // 输出文件夹
+//       // includes: "_includes", // 模板文件存放目录
+//       // layouts: "_layouts" // 布局文件存放目录
+//     },
+//     passthroughFileCopy: true
+//   };
+// };
+const fs = require('fs');
+const path = require('path');
+
 module.exports = function (eleventyConfig) {
-  // 复制静态资源（CSS、JS）到输出目录
+  // 复制静态资源（CSS、JS、Images）到输出目录
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/images");
+
+  // 在构建之前删除 dist 目录中的不需要的文件
+  eleventyConfig.on('beforeBuild', () => {
+    const distDir = path.join(__dirname, 'dist');
+    const filesToDelete = ['index.html', 'news/index.html'];
+
+    filesToDelete.forEach(file => {
+      const filePath = path.join(distDir, file);
+      if (fs.existsSync(filePath)) {
+        fs.unlinkSync(filePath);
+      }
+    });
+  });
+
   return {
     dir: {
       input: "src",    // 输入文件夹
@@ -10,6 +42,7 @@ module.exports = function (eleventyConfig) {
       // includes: "_includes", // 模板文件存放目录
       // layouts: "_layouts" // 布局文件存放目录
     },
-    passthroughFileCopy: true
+    passthroughFileCopy: true,
+    pathPrefix: "/qiyuan.github.io/" // 替换为你的仓库名称
   };
 };
