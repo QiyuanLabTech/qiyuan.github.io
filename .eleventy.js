154853f5648a88ff577f115b3ba146c082a935ae
gitgitgitgithub1 <162154543+gitgitgitgithub1@users.noreply.github.com>
2025-03-21 15:49:52 +0800

    新增includes

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,10 @@
 module.exports = function (eleventyConfig) {
+  // 添加 Nunjucks 模板支持
+  eleventyConfig.setTemplateFormats([
+    "html",
+    "njk",
+    "md"
+  ]);
   // 复制静态资源（CSS、JS）到输出目录
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
@@ -7,9 +13,12 @@ module.exports = function (eleventyConfig) {
     dir: {
       input: "src",    // 输入文件夹
       output: "docs",  // 输出文件夹
-      // includes: "_includes", // 模板文件存放目录
+      includes: "_includes", // 模板文件存放目录
       // layouts: "_layouts" // 布局文件存放目录
     },
+    templateFormats: ["html", "njk"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
     passthroughFileCopy: true
   };
 };
